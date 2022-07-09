ba1dc276da9d306d6c57a780edd16bf3aa72fa48
Pavlo Gomeniuk <pavlogomeniuk@pavlos-macbook-pro.home>
2022-07-09 21:02:45 +0200

    files added

---
+++ mongoserver/models/Post.js
@@ -0,0 +1,17 @@
+import mongoose, {Schema} from 'mongoose';
+
+const PostSchema = new Schema(
+	{
+		title: String,
+		text: String,
+		backgroundURL: String
+	},
+	{
+		timestaps: true
+	}
+
+);
+
+const Post = mongoose.model('Post', PostSchema);
+
+export default Post;
